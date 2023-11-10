34aa594bedd919183473e99079956ea549f6ae4e
Richard Remer <rich.remer@gmail.com>
2023-11-10 14:52:10 -0800

    user repository

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {default as configure} from "./lib/configure.js";
 export {User} from "./lib/user.js";
+export {UserRepository} from "./lib/user-repository.js";
 export {createRegistration, generateId} from "./lib/passkey.js";
